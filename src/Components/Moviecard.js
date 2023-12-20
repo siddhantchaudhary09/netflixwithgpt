2b8e5b7cfd312700c263af6a0e07ed4b6fc7681e
siddhant <94710682+siddhantchaudhary09@users.noreply.github.com>
2023-12-20 19:11:17 +0530

    posterpath fixed\

--- src/Components/Moviecard.js
+++ src/Components/Moviecard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { IMG_cdn } from "../utils/constants";
 
 const Moviecard = ({ id }) => {
+  if (!id) return;
   return (
     <div className=" w-28 sm:w-52  ">
       <img src={IMG_cdn + id} className="" />
